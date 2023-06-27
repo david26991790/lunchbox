ed64890ab609d1afbda8d1612d51b74ee7c986ef
david26991790 <128453780+david26991790@users.noreply.github.com>
2023-06-28 00:48:20 +0800

    20230628

--- app/src/main/java/com/example/lunchbox/MainPage.kt
+++ app/src/main/java/com/example/lunchbox/MainPage.kt
@@ -1,6 +1,5 @@
 package com.example.lunchbox
 
-import BackgroundMusicService
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -9,18 +8,14 @@ import android.view.View
 import androidx.annotation.RequiresApi
 
 class MainPage : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startBackgroundMusicService()
+
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun startBackgroundMusicService() {
-        val backgroundMusicServiceIntent = Intent(this, BackgroundMusicService::class.java)
-        startForegroundService(backgroundMusicServiceIntent)
-    }
+
 
     fun go_random() {
         startActivity(Intent(this, Lunchrandom::class.java))
@@ -38,12 +33,8 @@ class MainPage : AppCompatActivity() {
         go_school()
     }
 
-    fun go_lunch() {
-        startActivity(Intent(this, Lunchlist::class.java))
-    }
-
-    fun golunch(v: View) {
-        go_lunch()
+    fun goscroll(v: View) {
+        startActivity(Intent(this, ScrollActivity::class.java))
     }
 
     fun go_auth() {
